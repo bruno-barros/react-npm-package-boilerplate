b7937e18a84ffba6fccb20cff16bdd5ba5c35965
Bruno Barros <web@conceito-online.com.br>
2022-04-11 11:00:35 -0300

    testes com TS, fiz eslint

--- src/MyComponent/MyComponent.tsx
+++ src/MyComponent/MyComponent.tsx
@@ -11,13 +11,13 @@ export default function MyComponent (props: MyComponentProps) {
 
   const { name } = props;
 
-    return (
-      <div className='my-component'>
-        <i className='icon-test'>One</i><br></br>
-        <i className='icon-test'>Two</i><br></br>
-        <i className='icon-test'>Three</i> <br/>
-        <div className='name-holder'>My name is - {name}</div>
-      </div>
-    )
+  return (
+    <div className='my-component'>
+      <i className='icon-test'>One</i><br></br>
+      <i className='icon-test'>Two</i><br></br>
+      <i className='icon-test'>Three</i> <br/>
+      <div className='name-holder'>My name is - {name}</div>
+    </div>
+  )
   
 }
